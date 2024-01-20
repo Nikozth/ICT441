86314be5a6d37c808664f59116fafbb530f818b5
Nikozth <127218531+Nikozth@users.noreply.github.com>
2024-01-20 13:12:15 +0800

    Add files via upload

--- js/register.js
+++ js/register.js
@@ -3,21 +3,25 @@ function validateform(){
     var password=document.myform.password.value; 
     var contact=document.myform.contact.value;  
     var email=document.myform.email.value;  
-      
+    （These lines retrieve values from the form inputs (name, password, contact, and email) and assign them to corresponding variables.）
     if (name==null || name==""){  
       alert("Name can't be blank");  
       return false;  
     }
+    （This block checks if the 'name' variable is either null or an empty string. If true, it displays an alert and returns false, indicating that the form validation failed.）
     if (contact==null || contact==""){  
       alert("contact information can't be blank");  
       return false;  
     }
+    （Similar to the name validation, this block checks if the 'contact' variable is either null or an empty string. If true, it displays an alert and returns false.）
     if (email==null || email==""){  
       alert("E-mail can't be blank");  
       return false;  
     }
+    （Similar to the previous validations, this block checks if the 'email' variable is either null or an empty string. If true, it displays an alert and returns false.）
     if(password.length<6){  
       alert("Password must be at least 6 characters long.");  
       return false;  
       }  
+      （This block checks if the length of the 'password' variable is less than 6 characters. If true, it displays an alert and returns false.）
     }  
