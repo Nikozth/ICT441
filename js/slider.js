3fa76fd27c5e9535d5a6e5ad2266a3cd1be3afbd
Nikozth <127218531+Nikozth@users.noreply.github.com>
2024-01-23 19:36:19 +0800

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-//Add this code to your js file
+// 在 js 文件中添加这些代码
 let currentIndex = 0;
 const slides = document.querySelectorAll('.slide');
 
@@ -12,29 +12,26 @@ function nextSlide() {
   showSlide(currentIndex);
 }
 
-// initialize
+// 初始化
 showSlide(currentIndex);
 
-// Autoplay (switch images every 3 seconds)
+// 自动播放（每隔3秒切换一张图片）
 setInterval(nextSlide, 3000);
-(Initializes the slide index)
 let slideIndex = 1;
-// Switch the slide forward or backward
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-// Switch to a slide with a specific index
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-// Shows slides for a specific index
+
 function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName("slide");
-  // The processing loop switches, returning to the first slide if you exceed the number of slides
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
-  // Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
